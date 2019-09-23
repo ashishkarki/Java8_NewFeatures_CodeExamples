0a81de23c3bb0411f2d06ab595ae4e1a344f2bee
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2019-09-23 12:56:42 +0545

    Added examples for BiConsumer and Predicate interfaces.

--- src/main/functionInterfaces/BiConsumerExample.java
+++ src/main/functionInterfaces/BiConsumerExample.java
@@ -1,4 +1,39 @@
 package main.functionInterfaces;
 
+import main.databases.PersonDatabase;
+import main.models.Person;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
 public class BiConsumerExample {
+
+    static List<Person> personList = PersonDatabase.getPersonList();
+    static BiConsumer<String, List<String>> printerBiConsumer = (name, activities) -> System.out.println(name + " is active in :" + activities);
+
+    /**
+     * A simple example of using BiConsumer
+     */
+    public static void performArithmetic(Integer one, Integer two) {
+        BiConsumer<Integer, Integer> multiply = (a, b) -> System.out.println("Multiplication result is :" + (a * b));
+
+        BiConsumer<Integer, Integer> divide = (a, b) -> System.out.println("Division result is : " + (a / b));
+
+        multiply.andThen(divide).accept(one, two);
+    }
+
+    /**
+     * print name and activities of Person list (similar to {@code ConsumerExample})
+     */
+    public static void printNameAndActivities() {
+        System.out.println("Printing the person's name and activities using BiConsumer Interface");
+        personList.forEach(person -> printerBiConsumer.accept(person.getName(), person.getActivities())); // this is called consumer chaining
+    }
+
+
+    public static void main(String[] args) {
+        performArithmetic(10, 5);
+
+        printNameAndActivities();
+    }
 }
--- src/main/functionInterfaces/PredicateExample.java
+++ src/main/functionInterfaces/PredicateExample.java
@@ -1,4 +1,38 @@
 package main.functionInterfaces;
 
+import java.util.function.Predicate;
+
 public class PredicateExample {
+
+    static Predicate<Integer> checkIfEvenPredicate = n -> n % 2 == 0;
+    static Predicate<Integer> checkIfDivisibleByFive = n -> n % 5 == 0;
+
+    public static boolean simplePredicate(Integer num) {
+        return checkIfEvenPredicate.test(num);
+    }
+
+    public static boolean usingPredicateAnd(Integer num) {
+        return checkIfEvenPredicate.and(checkIfDivisibleByFive).test(num); // predicate chaining with 'and'
+    }
+
+    public static boolean usingPredicateOr(Integer num) {
+        return checkIfEvenPredicate.or(checkIfDivisibleByFive).test(num); // predicate chaining with 'or'
+    }
+
+    // negate reverses the result
+    public static boolean usingPredicateOrWithNegate(Integer num) {
+        return checkIfEvenPredicate.or(checkIfDivisibleByFive).negate().test(num); // this is originally  true; negate makes it false
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Checking if 4 is even: " + simplePredicate(4));
+
+        System.out.println("Checking if 10 is divisible by 2 and 5: " + usingPredicateAnd(10));
+        System.out.println("Checking if 4 is divisible by 2 and 5: " + usingPredicateAnd(4));
+
+        System.out.println("Checking if 10 is divisible by 2 or 5: " + usingPredicateOr(10));
+        System.out.println("Checking if 4 is divisible by 2 or 5: " + usingPredicateOr(4));
+
+        System.out.println("Checking if 4 is divisible by 2 or 5 appended with a negate: " + usingPredicateOrWithNegate(4));
+    }
 }
